6b1e78b2734d3a96d6b413e9ce731d319bc6255c
Teen <117570251+sodelight26@users.noreply.github.com>
2024-04-30 19:15:15 +0700

    Update firebaseDb.js

--- database/firebaseDb.js
+++ database/firebaseDb.js
@@ -2,13 +2,13 @@ import firebase from 'firebase/app';
 import 'firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCJ1m91L37D4wzONF1WABIOFbYtS11wkiM",
+    apiKey: "",
     authDomain: "react-native-ie.firebaseapp.com",
     projectId: "react-native-ie",
-    storageBucket: "react-native-ie.appspot.com",
-    messagingSenderId: "2261357178",
-    appId: "1:2261357178:web:35a4bf6061e8c6ebcb61c3",
-    measurementId: "G-998M6TV072"
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
 };
 
 // Initialize Firebase
